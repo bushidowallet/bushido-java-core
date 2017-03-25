adbd3c798076f2c20c929188fe845e849fe39a6d
Robert Jesionek <yesionuk@gmail.com>
2017-03-25 21:02:42 +0100

    changed implementation of toDER function

--- src/main/java/com/bushidowallet/core/bitcoin/ecdsa/ECDSASignature.java
+++ src/main/java/com/bushidowallet/core/bitcoin/ecdsa/ECDSASignature.java
@@ -1,7 +1,10 @@
 package com.bushidowallet.core.bitcoin.ecdsa;
 
-import com.bushidowallet.core.bitcoin.util.ByteWriter;
+import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.DERSequenceGenerator;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
 
 /**
@@ -21,54 +24,25 @@ public class ECDSASignature {
         this.compressed = compressed;
     }
 
-    public byte[] toBytes() {
-        byte[] rnbufbase = r.toByteArray();
-        byte[] rnbuf = new byte[32];
-        byte[] snbuf = s.toByteArray();
-        //this is basically removing the leading 0 byte, need to test that extensively, did that to be 100% in sync with bitcore.js
-        System.arraycopy(rnbufbase, 1, rnbuf, 0, rnbufbase.length - 1);
-
-        //todo: test this extensively
-        boolean rneg = (rnbuf[0] & 0x80) != 0;
-        boolean sneg = (snbuf[0] & 0x80) != 0;
-
-        byte[] zero = new byte[1];
-        zero[0] = 0x00;
-
-        byte[] drn = new byte[rnbuf.length + 1];
-        System.arraycopy(zero, 0, drn, 0, 1);
-        System.arraycopy(rnbuf, 0, drn, 1, rnbuf.length);
-
-        byte[] dsn = new byte[snbuf.length + 1];
-        System.arraycopy(zero, 0, dsn, 0, 1);
-        System.arraycopy(snbuf, 0, dsn, 1, snbuf.length);
-
-        byte[] rbuf = rneg == true ? drn : rnbuf;
-        byte[] sbuf = sneg == true ? dsn : snbuf;
-
-        int rlength = rbuf.length;
-        int slength = sbuf.length;
-        int len = 2 + rlength + 2 + slength;
-        int rheader = 0x02;
-        int sheader = 0x02;
-        int header = 0x30;
-
-        ByteWriter writer = new ByteWriter(0);
-        writer.putUInt8(header);
-        writer.putUInt8(len);
-        writer.putUInt8(rheader);
-        writer.putUInt8(rlength);
-        writer.putBytes(rbuf);
-        writer.putUInt8(sheader);
-        writer.putUInt8(slength);
-        writer.putBytes(sbuf);
-
-        return writer.toBytes();
+    public byte[] toDER() {
+        return toDER(r, s);
     }
 
-    //DER signature format: [30] [total len] [02] [len R] [R] [02] [len S] [S]
-    public byte[] toDER() {
-        return toBytes();
+    private static byte[] toDER(BigInteger r, BigInteger s) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(72);
+        DERSequenceGenerator seq = null;
+        byte[] res = new byte[0];
+        try {
+            seq = new DERSequenceGenerator(bos);
+            seq.addObject(new ASN1Integer(r));
+            seq.addObject(new ASN1Integer(s));
+            seq.close();
+            res = bos.toByteArray();
+            return res;
+        } catch (IOException e) {
+
+        }
+        return null;
     }
 }
 
